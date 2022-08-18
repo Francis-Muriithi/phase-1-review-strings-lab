6ebf0864222216785aaea5ddbafeb6641cefbbc3
Francis <mufraggie@gmail.com>
2022-08-18 07:27:37 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = "Francis Muriithi";
+const welcomeMessage = "Welcome to Flatbook, Francis Muriithi!";
+const excitedWelcomeMessage ="WELCOME TO FLATBOOK, FRANCIS MURIITHI!";
+const shortGreeting = "Welcome, " + "F!";
+
