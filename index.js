0b597741845eb093cf4a70f0c5e6a4a85cfce113
Francis <mufraggie@gmail.com>
2022-08-20 11:21:38 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Write your code in this file!
 const currentUser = "Francis Muriithi";
-const welcomeMessage = "Welcome to Flatbook, Francis Muriithi!";
-const excitedWelcomeMessage ="WELCOME TO FLATBOOK, FRANCIS MURIITHI!";
-const shortGreeting = "Welcome, " + "F!";
-
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage =welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, (${currentUser.charAt(0)}!`;
